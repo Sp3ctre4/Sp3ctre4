242883243c6424d1eb142755a3fa358b29190046
Sp3ctre4 <pshughes1@liberty.edu>
2025-02-09 17:36:56 -0500

    Update SpawnMinions.ps1
    eeehh.. maube?

--- AD_Malfeasance/SpawnMinions.ps1
+++ AD_Malfeasance/SpawnMinions.ps1
@@ -3,3 +3,7 @@
   An example CSV File is provided in this repo.
 #>
 
+# put the path to the csv file here
+$import_users = Import-Csv -Path c:\minionexample.csv
+
+$import_users | ForEach-Object {New-ADUser -Name $($_.First + " " + $_.Last) -GivenName $_.First -Surname $_.Last -Department $_.Department -EmployeeID $_.EmployeeID -DisplayName $($_.First + " " + $_.Last) -UserPrincipalName $_.UserPrincipalName -SamAccountName $_.samAccountName -AccountPassword $(ConvertTo-SecureString $_.Password -AsPlainText -Force) -Title $_.Title -Path $_.OU -Enabled $True}
